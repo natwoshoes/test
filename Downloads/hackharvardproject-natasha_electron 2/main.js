79841e6c8e89d3bbaed378a4d62c3c9c1ade1f96
natasha <mnatasha737@gmail.com>
2025-10-04 17:10:40 -0400

    adding main.js

---
+++ Downloads/hackharvardproject-natasha_electron 2/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('index.html');
+
+  // Open DevTools (optional, remove in production)
+  mainWindow.webContents.openDevTools();
+}
+
+// When Electron is ready, create the window
+app.whenReady().then(() => {
+  createWindow();
+
+  // On macOS, re-create window when dock icon is clicked
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
